e163be8aec269edb898bf2f62e35480b938c7a2b
csAfter40 <csafter40@gmail.com>
2024-02-21 13:16:18 +0300

    feat: add header bar

--- components/navigators/MainStack.js
+++ components/navigators/MainStack.js
@@ -5,14 +5,21 @@ import SignUp from "../../screens/SignUp";
 import Home from "../../screens/Home";
 import Search from "../../screens/Search";
 import Messages from "../../screens/Messages";
-import LoadingOverlay from "../../screens/LoadingOverlay";
 import { AuthContext } from "../../providers/AuthProvider";
+import TopBar from "../TopBar";
 
 const Stack = createNativeStackNavigator();
 
 function AuthStack() {
 	return (
-		<Stack.Navigator>
+		<Stack.Navigator
+			screenOptions={{
+				headerTitleAlign: "center",
+				animation: "slide_from_right",
+				header: (props) => <TopBar {...props} />,
+				contentStyle: { backgroundColor: null },
+			}}
+		>
 			<Stack.Screen name="SignIn" component={SignIn} />
 			<Stack.Screen name="SignUp" component={SignUp} />
 		</Stack.Navigator>
@@ -20,8 +27,15 @@ function AuthStack() {
 }
 function AuthenticatedStack() {
 	return (
-		<Stack.Navigator>
-			<Stack.Screen name="Home" component={Home} />
+		<Stack.Navigator
+			screenOptions={{
+				headerTitleAlign: "center",
+				animation: "slide_from_right",
+				header: (props) => <TopBar {...props} />,
+				contentStyle: { backgroundColor: null },
+			}}
+		>
+			<Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
 			<Stack.Screen name="Search" component={Search} />
 			<Stack.Screen name="Messages" component={Messages} />
 		</Stack.Navigator>
