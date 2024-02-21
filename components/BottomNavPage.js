ee9560784e086054546566f9ea767039a51b01ea
csAfter40 <csafter40@gmail.com>
2024-02-21 13:15:04 +0300

    feat: add BottomNavPage component

---
+++ components/BottomNavPage.js
@@ -0,0 +1,22 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { useTheme } from "react-native-paper";
+import TopBar from "./TopBar";
+
+export default function BottomNavPage({ children, style, title }) {
+	const theme = useTheme();
+	const styles = StyleSheet.create({
+		page: {
+			flex: 1,
+			padding: 20,
+			width: "100%",
+			backgroundColor: theme.colors.background,
+		},
+	});
+	return (
+		<>
+			<TopBar title={title} />
+			<View style={[styles.page, style]}>{children}</View>
+		</>
+	);
+}
