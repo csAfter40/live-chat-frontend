dd5813a9ef864ec838f1261e04990bd55ba02a8a
csAfter40 <csafter40@gmail.com>
2024-02-21 13:15:28 +0300

    feat: add TopBar component

---
+++ components/TopBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Appbar } from "react-native-paper";
+import { getHeaderTitle } from "@react-navigation/elements";
+
+export default function TopBar({ title, navigation, route, options, back }) {
+	const barTitle = title || getHeaderTitle(options, route.name);
+	return (
+		<Appbar.Header elevated={true} mode="center-aligned">
+			{back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+			<Appbar.Content title={barTitle} />
+		</Appbar.Header>
+	);
+}
