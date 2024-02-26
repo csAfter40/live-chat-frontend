be172d358e91cff8bd29039945cb49c870cedd75
csAfter40 <csafter40@gmail.com>
2024-02-26 23:23:08 +0300

    feat: implement image picker for thumbnail picture

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,19 +1,42 @@
-import { StyleSheet, Image } from "react-native";
+import { StyleSheet, Image, View } from "react-native";
 import React from "react";
 import BottomNavPage from "../components/BottomNavPage";
-import { Button, Text, useTheme } from "react-native-paper";
+import { Button, Text, IconButton, useTheme } from "react-native-paper";
 import useGlobal from "../global";
+import * as ImagePicker from "expo-image-picker";
 
 export default function Profile() {
 	theme = useTheme();
 	const logout = useGlobal((state) => state.logout);
 	const user = useGlobal((state) => state.user);
+	async function handleImagePick() {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.All,
+			base64: true,
+		});
+		if (result.canceled) {
+			return;
+		}
+		const file = result.assets[0];
+		console.log(file);
+	}
 	return (
 		<BottomNavPage style={styles.page} title={"Profile"}>
-			<Image
-				style={styles.image}
-				source={require("../assets/images/user-avatar.jpg")}
-			/>
+			<View>
+				<Image
+					style={styles.image}
+					source={require("../assets/images/user-avatar.jpg")}
+				/>
+				<IconButton
+					style={[styles.iconButton, { borderColor: theme.colors.background }]}
+					icon="pencil"
+					containerColor={theme.colors.secondaryContainer}
+					iconColor={theme.colors.secondary}
+					size={25}
+					onPress={handleImagePick}
+					mode="outlined"
+				/>
+			</View>
 			<Text variant="titleLarge">{user.full_name}</Text>
 			<Text variant="bodyLarge">@{user.username}</Text>
 			<Button
@@ -42,5 +65,11 @@ const styles = StyleSheet.create({
 	},
 	button: {
 		marginTop: 30,
+	},
+	iconButton: {
+		position: "absolute",
+		top: 0,
+		right: 0,
+		borderWidth: 4,
 	},
 });
