3271b3d14fa0e68d7847b07d2d8a3d940efec534
csAfter40 <csafter40@gmail.com>
2024-02-28 22:54:58 +0300

    feat: add search bar

--- screens/Search.js
+++ screens/Search.js
@@ -1,18 +1,28 @@
 import { Text, StyleSheet } from "react-native";
 import React from "react";
 import Page from "../components/Page";
+import { Searchbar, Divider } from "react-native-paper";
+import Message from "../components/Message";
 
 export default function Search() {
+	const [results, setResults] = React.useState([]);
+	const [searchQuery, setSearchQuery] = React.useState("");
 	return (
 		<Page style={styles.page}>
-			<Text>Search</Text>
+			<Searchbar
+				placeholder="Search..."
+				onChangeText={setSearchQuery}
+				value={searchQuery}
+			/>
+			<Divider style={{ width: "100%", margin: 20 }} />
+			<Message message={"No results found"} />
 		</Page>
 	);
 }
 
 const styles = StyleSheet.create({
 	page: {
-		justifyContent: "center",
+		justifyContent: "flex-start",
 		alignItems: "center",
 	},
 });
