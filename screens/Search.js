d1747560eadf63428a0b0448c94a32f5475a7ddb
csAfter40 <csafter40@gmail.com>
2024-03-01 00:19:51 +0300

    feat: setup search bar logic

--- screens/Search.js
+++ screens/Search.js
@@ -1,21 +1,34 @@
-import { Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import React from "react";
 import Page from "../components/Page";
-import { Searchbar, Divider } from "react-native-paper";
+import { Searchbar } from "react-native-paper";
 import Message from "../components/Message";
+import PeopleList from "../components/PeopleList";
+import useGlobal from "../global";
 
 export default function Search() {
-	const [results, setResults] = React.useState([]);
+	const searchResults = useGlobal((state) => state.searchResults);
+	const searchUser = useGlobal((state) => state.searchUser);
 	const [searchQuery, setSearchQuery] = React.useState("");
+	React.useEffect(() => {
+		searchUser(searchQuery);
+	}, [searchQuery]);
 	return (
 		<Page style={styles.page}>
 			<Searchbar
+				style={styles.searchBar}
 				placeholder="Search..."
 				onChangeText={setSearchQuery}
 				value={searchQuery}
+				autoCapitalize="none"
 			/>
-			<Divider style={{ width: "100%", margin: 20 }} />
-			<Message message={"No results found"} />
+			{searchResults === null ? (
+				<Message icon="magnify" message={"Search for friends"} />
+			) : searchResults.length === 0 ? (
+				<Message message={`No results found for ${searchQuery}`} />
+			) : (
+				<PeopleList people={searchResults} />
+			)}
 		</Page>
 	);
 }
@@ -24,5 +37,8 @@ const styles = StyleSheet.create({
 	page: {
 		justifyContent: "flex-start",
 		alignItems: "center",
+	},
+	searchBar: {
+		marginBottom: 20,
 	},
 });
