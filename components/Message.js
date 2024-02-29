c907bcd4e54d80440cd931cb46750d41bec60715
csAfter40 <csafter40@gmail.com>
2024-02-29 21:31:07 +0300

    fix: styling

--- components/Message.js
+++ components/Message.js
@@ -6,28 +6,30 @@ import { useTheme } from "react-native-paper";
 export default function Message({ message, style, textStyle, icon }) {
 	const theme = useTheme();
 	return (
-		<View
-			style={[styles.container, { color: theme.colors.tertiaryContainer }, style]}
-		>
+		<View style={[styles.container, style]}>
 			<Icon
-				source={icon || "alert-outline"}
-				color={theme.colors.tertiary}
-				size={20}
+				source={icon || "alert-rhombus"}
+				color={theme.colors.secondary}
+				size={60}
 			/>
-			<Text style={[styles.text, textStyle]}>{message}</Text>
+			<Text style={[styles.text, { color: theme.colors.secondary }, textStyle]}>
+				{message}
+			</Text>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flexDirection: "row",
+		flex: 1,
 		width: "100%",
 		alignItems: "center",
 		justifyContent: "flex-start",
-		borderRadius: 4,
+		borderRadius: 8,
+		height: 40,
+		padding: 30,
 	},
 	text: {
-		marginLeft: 10,
+		marginTop: 10,
 	},
 });
