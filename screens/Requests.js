afa6dd8d95e0f9480e1281bb3a05e8a5d146860a
csAfter40 <csafter40@gmail.com>
2024-03-02 21:38:21 +0300

    feat: add Requests screen

--- screens/Requests.js
+++ screens/Requests.js
@@ -1,11 +1,30 @@
-import { Text, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
 import BottomNavPage from "../components/BottomNavPage";
+import useGlobal from "../global";
+import LoadingOverlay from "./LoadingOverlay";
+import Message from "../components/Message";
+import RequestList from "../components/RequestList";
 
 export default function Requests() {
+	const requestsList = useGlobal((state) => state.requestsList);
+	console.log("requsets list: ", requestsList);
+	if (requestsList === null) {
+		return (
+			<BottomNavPage style={styles.page} title={"Requests"}>
+				<LoadingOverlay />
+			</BottomNavPage>
+		);
+	}
 	return (
 		<BottomNavPage style={styles.page} title={"Requests"}>
-			<Text>Requests</Text>
+			{requestsList === null ? (
+				<LoadingOverlay />
+			) : requestsList.length === 0 ? (
+				<Message message="You currently don't have any friend requests." />
+			) : (
+				<RequestList requests={requestsList} />
+			)}
 		</BottomNavPage>
 	);
 }
