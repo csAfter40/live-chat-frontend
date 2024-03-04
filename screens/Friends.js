54c73335b2efba556d4819c971ba89742f0de2b5
csAfter40 <csafter40@gmail.com>
2024-03-04 15:39:56 +0300

    feat: add friends screen

--- screens/Friends.js
+++ screens/Friends.js
@@ -1,11 +1,29 @@
-import { Text, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
 import BottomNavPage from "../components/BottomNavPage";
+import useGlobal from "../global";
+import LoadingOverlay from "./LoadingOverlay";
+import Message from "../components/Message";
+import FriendList from "../components/FriendList";
 
 export default function Friends() {
+	const friends = useGlobal((state) => state.friendsList);
+	// if (friendsList === null) {
+	// 	return (
+	// 		<BottomNavPage style={styles.page} title={"Friends"}>
+	// 			<LoadingOverlay />
+	// 		</BottomNavPage>
+	// 	);
+	// }
 	return (
-		<BottomNavPage style={styles.page} title="Friends">
-			<Text>Friendss</Text>
+		<BottomNavPage style={styles.page} title={"Friends"}>
+			{friends === null ? (
+				<LoadingOverlay />
+			) : friends.length === 0 ? (
+				<Message message="No connections available" />
+			) : (
+				<FriendList friends={friends} />
+			)}
 		</BottomNavPage>
 	);
 }
