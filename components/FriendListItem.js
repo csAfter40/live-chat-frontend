c781a358d88d659482f8fff59363ef8958cbf6a3
csAfter40 <csafter40@gmail.com>
2024-03-05 15:00:35 +0300

    feat: add preview text and time

--- components/FriendListItem.js
+++ components/FriendListItem.js
@@ -1,27 +1,27 @@
 import React from "react";
-import { List, Avatar, Divider, Text, Button } from "react-native-paper";
+import { View } from "react-native";
+import { List, Avatar, Divider, Text } from "react-native-paper";
 import { getThumbnail } from "../utils";
-import useGlobal from "../global";
+import { formatTime } from "../utils";
+import { useNavigation } from "@react-navigation/native";
 
 export default function FriendListItem({ friendData }) {
+	const navigation = useNavigation();
 	const friend = friendData.friend;
 	return (
 		<>
 			<List.Item
+				onPress={() => navigation.navigate("Messages", friendData)}
 				title={friend.full_name}
 				left={() => (
 					<Avatar.Image size={50} source={getThumbnail(friend.thumbnail)} />
 				)}
-				// description={() => <Text>{time}</Text>}
-				// right={() => (
-				// 	<Button
-				// 		style={{ alignSelf: "center" }}
-				// 		mode="contained-tonal"
-				// 		onPress={handleAccept}
-				// 	>
-				// 		Accept
-				// 	</Button>
-				// )}
+				description={() => <Text>{friendData.preview}</Text>}
+				right={() => (
+					<Text style={{ alignSelf: "center", color: "#707070" }}>
+						{formatTime(friendData.updated)}
+					</Text>
+				)}
 			/>
 			<Divider />
 		</>
