90c9f9876c1491060093e6b4048ee23c0c0d5b0f
csAfter40 <csafter40@gmail.com>
2024-03-05 15:00:00 +0300

    feat: add time formatter utility function

--- utils.js
+++ utils.js
@@ -7,3 +7,33 @@ export function getThumbnail(url) {
 	}
 	return { uri: `http://${ADDRESS}${url}` };
 }
+
+export function formatTime(date) {
+	if (date === null) {
+		return "-";
+	}
+	const now = new Date();
+	const timeDelta = Math.abs(now - new Date(date)) / 1000; //seconds
+
+	if (timeDelta < 60) {
+		return "now";
+	} else if (timeDelta < 60 * 60) {
+		const mins = Math.floor(timeDelta / 60);
+		return `${mins}m ago`;
+	} else if (timeDelta < 60 * 60 * 24) {
+		const hours = Math.floor(timeDelta / (60 * 60));
+		return `${hours}h ago`;
+	} else if (timeDelta < 60 * 60 * 24 * 7) {
+		const days = Math.floor(timeDelta / (60 * 60 * 24));
+		return `${days}d ago`;
+	} else if (timeDelta < 60 * 60 * 24 * 31) {
+		const weeks = Math.floor(timeDelta / (60 * 60 * 24 * 7));
+		return `${weeks}w ago`;
+	} else if (timeDelta < 60 * 60 * 24 * 365) {
+		const months = Math.floor(timeDelta / (60 * 60 * 24 * 31));
+		return `${months}mo ago`;
+	} else {
+		const years = Math.floor(timeDelta / (60 * 60 * 24 * 365));
+		return `${years}y ago`;
+	}
+}
