3e2a5d59a5e3eb2165d6b7f95577601c6a9eca9a
csAfter40 <csafter40@gmail.com>
2024-03-06 14:00:38 +0300

    feat: add Messages screen

--- screens/Messages.js
+++ screens/Messages.js
@@ -1,11 +1,58 @@
-import { Text, StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Text, Avatar, Appbar, TextInput, IconButton } from "react-native-paper";
 import React from "react";
 import Page from "../components/Page";
+import { getThumbnail } from "../utils";
+import useGlobal from "../global";
 
-export default function Messages() {
+function MessagesTitle({ friend }) {
+	return (
+		<View style={styles.messageTitleContainer}>
+			<Avatar.Image size={24} source={getThumbnail(friend.thumbnail)} />
+			<Appbar.Content title={friend.full_name} />
+		</View>
+	);
+}
+
+export default function Messages({ navigation, route }) {
+	const messageSend = useGlobal((state) => state.messageSend);
+	const friend = route.params.friend;
+	const connectionId = route.params.id;
+	const [messageText, setMessageText] = React.useState("");
+	React.useLayoutEffect(() => {
+		navigation.setOptions({
+			headerTitle: (props) => <MessagesTitle {...props} friend={friend} />,
+		});
+	}, []);
+	function onSend() {
+		const cleanedText = messageText.replace(/\s+/g, " ").trim();
+		if (cleanedText.length === 0) return;
+		messageSend(connectionId, cleanedText);
+		setMessageText("");
+	}
 	return (
 		<Page style={styles.page}>
-			<Text>Messages</Text>
+			<View style={styles.messagesContainer}></View>
+			<TextInput
+				multiline={true}
+				mode="outlined"
+				value={messageText}
+				onChangeText={setMessageText}
+				style={styles.messageInput}
+				outlineStyle={{ borderRadius: 10 }}
+				label="Write message"
+				right={
+					<TextInput.Icon
+						icon="send-circle"
+						iconColor="#689f38"
+						// iconColor={theme.colors.primary}
+						size={40}
+						onPress={onSend}
+						style={{ alignSelf: "center" }}
+						disabled={messageText === ""}
+					/>
+				}
+			/>
 		</Page>
 	);
 }
@@ -14,5 +61,21 @@ const styles = StyleSheet.create({
 	page: {
 		justifyContent: "center",
 		alignItems: "center",
+	},
+	messageTitleContainer: {
+		flexDirection: "row",
+		gap: 15,
+		alignItems: "center",
+	},
+	messagesContainer: {
+		flex: 1,
+		borderColor: "red",
+		borderWidth: 2,
+		width: "100%",
+	},
+	messageInput: {
+		width: "100%",
+		marginVertical: 20,
+		maxHeight: 100,
 	},
 });
